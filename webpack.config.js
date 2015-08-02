58b62502e2dd6c015f33e501f869b58d3b1fd671
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2015-08-02 10:36:53 +1000

    Initial commit with game

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+// webpack.config.js
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: [
+    'webpack/hot/dev-server',
+    './src/index.js'
+  ],
+  output: {
+    path: './public/',
+    publicPath: "/assets/",
+    filename: 'js/bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules|bower_components/,
+        loader: "babel-loader",
+        query: {
+          optional: ["es7.decorators", "es7.classProperties"]
+        }
+      },
+      {
+        test: /\.scss|css$/,
+        loader: ExtractTextPlugin.extract(
+          // activate source maps via loader query
+          'css?sourceMap!' +
+          'sass?sourceMap'
+        )
+      }
+    ]
+  },
+  plugins: [
+    // extract inline css into separate 'styles.css'
+    new ExtractTextPlugin('css/styles.css')
+  ]
+};
