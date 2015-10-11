b7568afd2f419f1cfc04f2896df35fc7a2c3cf0c
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2015-10-11 15:37:06 +1100

    Add Test around horizontal win

--- test/spec/TicTacToe/Components/Board-spec.js
+++ test/spec/TicTacToe/Components/Board-spec.js
@@ -1,6 +1,37 @@
-describe('TicTacToe', () => {
-    it('has a test', () => {
-        expect(1).toBe(2);
+var React = require('react/addons')
+var Board = require('../../../../src/js/TicTacToe/Components/Board');
+var Player = require('../../../../src/js/TicTacToe/Models/Player');
+
+describe('Board', () => {
+    var Component;
+    var ReactTestUtils = React.addons.TestUtils;
+    var players;
+    beforeEach(() => {
+         players = {
+            player1: new Player({
+                name: "Player 1",
+                marker: "x",
+                color: "red"
+            }),
+            player2: new Player({
+                name: "Player 2",
+                marker: "o",
+                color: "blue"
+            })
+        };
+        Component = ReactTestUtils.renderIntoDocument(<Board {...players}/>);
     });
+
+    describe('picking a winner', () => {
+        it('win by top row', () => {
+            Component.setPlayersMove([0,0]);
+            Component.setPlayersMove([1,0]);
+            Component.setPlayersMove([0,1]);
+            Component.setPlayersMove([2,0]);
+            Component.setPlayersMove([0,2]);
+            expect(Component.state.winner).toBe(players.player1);
+        });
+
+    })
 });
 
