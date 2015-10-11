f599e08234d36cd60c42de015c7ab7c6da3f5830
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2015-10-11 16:41:19 +1100

    Add test for winning the game

--- test/spec/TicTacToe/Components/Board-spec.js
+++ test/spec/TicTacToe/Components/Board-spec.js
@@ -22,8 +22,8 @@ describe('Board', () => {
         Component = ReactTestUtils.renderIntoDocument(<Board {...players}/>);
     });
 
-    describe('picking a winner', () => {
-        it('win by top row', () => {
+    describe('win by row', function() {
+        it('top', function(){
             Component.setPlayersMove([0,0]);
             Component.setPlayersMove([1,0]);
             Component.setPlayersMove([0,1]);
@@ -32,6 +32,72 @@ describe('Board', () => {
             expect(Component.state.winner).toBe(players.player1);
         });
 
-    })
+        it('middle', function(){
+            Component.setPlayersMove([1,0]);
+            Component.setPlayersMove([0,0]);
+            Component.setPlayersMove([1,1]);
+            Component.setPlayersMove([0,1]);
+            Component.setPlayersMove([1,2]);
+            expect(Component.state.winner).toBe(players.player1);
+        });
+
+        it('bottom', function(){
+            Component.setPlayersMove([2,0]);
+            Component.setPlayersMove([0,0]);
+            Component.setPlayersMove([2,1]);
+            Component.setPlayersMove([0,1]);
+            Component.setPlayersMove([2,2]);
+            expect(Component.state.winner).toBe(players.player1);
+        });
+    });
+
+    describe('win by column', function () {
+        it('left', function () {
+            Component.setPlayersMove([0,0]);
+            Component.setPlayersMove([1,1]);
+            Component.setPlayersMove([0,1]);
+            Component.setPlayersMove([1,2]);
+            Component.setPlayersMove([0,2]);
+            expect(Component.state.winner).toBe(players.player1);
+        });
+
+        it('middle', function () {
+            Component.setPlayersMove([1,0]);
+            Component.setPlayersMove([0,0]);
+            Component.setPlayersMove([1,1]);
+            Component.setPlayersMove([0,1]);
+            Component.setPlayersMove([1,2]);
+            expect(Component.state.winner).toBe(players.player1);
+        });
+
+        it('right', function () {
+            Component.setPlayersMove([2,0]);
+            Component.setPlayersMove([0,0]);
+            Component.setPlayersMove([2,1]);
+            Component.setPlayersMove([0,1]);
+            Component.setPlayersMove([2,2]);
+            expect(Component.state.winner).toBe(players.player1);
+        });
+    });
+
+    describe('win by cross section', function() {
+        it('top left to bottom right', function () {
+            Component.setPlayersMove([0,0]);
+            Component.setPlayersMove([0,2]);
+            Component.setPlayersMove([1,1]);
+            Component.setPlayersMove([0,1]);
+            Component.setPlayersMove([2,2]);
+            expect(Component.state.winner).toBe(players.player1);
+        });
+
+        it('bottom left to top right', function () {
+            Component.setPlayersMove([0,2]);
+            Component.setPlayersMove([0,0]);
+            Component.setPlayersMove([1,1]);
+            Component.setPlayersMove([0,1]);
+            Component.setPlayersMove([2,0]);
+            expect(Component.state.winner).toBe(players.player1);
+        });
+    });
 });
 
