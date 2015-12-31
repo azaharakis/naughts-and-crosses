e2b24eae63ac841cfb215397d6675bf7b322c1f2
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2015-12-31 13:06:22 +1100

    reduce boilerplate for debouce

--- src/js/util/debounce.js
+++ src/js/util/debounce.js
@@ -2,8 +2,6 @@ export default function debounce( fn, time) {
     let debouncedFn;
     return () => {
         clearTimeout(debouncedFn);
-        debouncedFn = setTimeout( () => {
-            fn();
-        }, time);
+        debouncedFn = setTimeout(fn, time);
     }
 }
